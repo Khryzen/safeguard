c7841990341e3c83ee0978df16ac63f2457d0eb4
Khryzen <mdeguzman.cpe@gmail.com>
2024-04-01 14:03:37 +0800

    Add protocols

--- models/protocols.go
+++ models/protocols.go
@@ -4,8 +4,12 @@ import "github.com/uadmin/uadmin"
 
 type Protocols struct {
 	uadmin.Model
-	Name        string
-	Code        string
-	Disasters   Disasters
-	DisastersID uint
+	Name         string
+	Code         string
+	Disasters    Disasters
+	DisastersID  uint
+	AlertLevel   AlertLevel
+	AlertLevelID uint
+	Description  string
+	Active       bool
 }

cd75225d1d43a881d239e95597a040eb286180db
Mark Bryan De Guzman <mdeguzman.cpe@gmail.com>
2024-06-12 15:05:31 +0800

    Add dark mode and light mode in mobile

--- static/js/mobile.js
+++ static/js/mobile.js
@@ -10,16 +10,20 @@ navItems.forEach(function (e, i) {
 
 $('#home').on('click', function(){
 	$('#main_body').load((window.location.href).split('/mobile/')[0] + '/mobile/dashboard' + ' #main_body');
+	window.location.href = '/mobile/dashboard/';
 })
 
 $('#enforcers').on('click', function(){
 	$('#main_body').load((window.location.href).split('/mobile/')[0] +'/mobile/enforcers'+ ' #main_body');
+	window.location.href = '/mobile/enforcers/';
 })
 
 $('#help').on('click', function () {
 	$('#main_body').load((window.location.href).split('/mobile/')[0] + '/mobile/help' + ' #main_body');
+	window.location.href = '/mobile/help/';
 })
 
 $('#about').on('click', function () {
 	$('#main_body').load((window.location.href).split('/mobile/')[0] + '/mobile/about' + ' #main_body');
+	window.location.href = '/mobile/about/';
 })

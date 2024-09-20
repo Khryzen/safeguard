8179cccdf4c6c4692985d83415ba558080b0f022
Khryzen <mdeguzman.cpe@gmail.com>
2024-09-20 15:38:42 +0800

    Send sms

--- models/incident_report.go
+++ models/incident_report.go
@@ -51,7 +51,7 @@ func (i *IncidentReport) Save() {
 		cell := ValidateCellPhone(sms_users[j].MobileNumber)
 		if cell != "" {
 			o.Phone = cell
-			// o.SendSMS()
+			o.SendSMS()
 		}
 		if sms_users[j].EmailAddress != "" {
 			// SendEmail(sms_users[j].EmailAddress, o.Message)
